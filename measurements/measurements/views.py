e2df16ff29b1d26b79ed708f307924a9981c8929
Sara Benavides <s.benavidesm@uniandes.edu.co>
2024-05-20 22:00:16 -0500

    corrección measurements check_place

--- measurements/measurements/views.py
+++ measurements/measurements/views.py
@@ -20,7 +20,7 @@ def check_place(data):
     r = requests.get(settings.PATH_VAR, headers={"Accept":"application/json"})
     places = r.json()
     for place in places:
-        if data["place"] == place["id"]:
+        if data["place"] == place["name"]:
             return True
     return False
 
